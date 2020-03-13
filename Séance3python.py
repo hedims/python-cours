0ebacdb3c34f6ca68f7a8fa8580e94477e0bfe99
hedims <58592216+hedims@users.noreply.github.com>
2020-03-13 13:00:09 +0100

    Add files via upload

---
+++ Séance3python.py
@@ -0,0 +1,23 @@
+class animal:
+
+  def __init__(self,name,age,sexe):
+    self.name=name
+    self.age=age
+    self.sexe=sexe
+
+animal1 = animal()
+print(animal1)
+
+animal2 = animal()
+print(animal2)
+
+animal3 = animal()
+print(animal3)
+
+animal4 = animal()
+print(animal4)
+
+list animaux[]
+list animaux.append(animal1)
+list animaux.append(animal2)
+list animaux.append(animal3)
